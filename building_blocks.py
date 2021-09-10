c3a00a8d424ad3ee1b05dd8cf0dc58b4c3e2252e
muddyrains <afuller187187@gmail.com>
2021-09-10 11:02:27 -0400

    starting to add type checking

--- building_blocks.py
+++ building_blocks.py
@@ -4,6 +4,9 @@ from torch import nn, einsum
 from einops import rearrange, repeat, reduce
 from positional_and_masking_utils import apply_rotary_pos_emb
 import math
+from torchtyping import TensorType, patch_typeguard
+from typeguard import typechecked
+from typing import Optional
 
 
 def exists(val):
@@ -171,7 +174,198 @@ working with very long sequences (i.e. in the thousands of tokens) - stick with 
 """
 
 
-class Attention(nn.Module):
+class NewAttention(nn.Module):
+    def __init__(self,
+                 dim,  # Input and output dimension size (typically it is d_model)
+                 attn_dim,  # Dimension size of attention (typically it is equal to dim)
+                 num_heads,  # Number of attention heads
+                 previous_attention_bool=False,  # Whether or not to re-use the last attention map
+                 pre_norm_bool=True,  # Apply layer normalization before attention
+                 post_norm_bool=False,  # Apply layer normalization after attention
+                 causal_mask_bool=True,  # Apply a causal mask (used for auto-regressive models)
+                 rotate_qk_bool=True,  # Apply a rotation to queries and keys, before their dot product
+                 rotate_v_bool=True,  # Apply a rotation to values
+                 ):
+        """
+        Standard attention function, with a few features. Lazy attention (set previous_attention_bool=True) allows us
+        to skip calculating a new attention map, and re-use the last attention map: https://arxiv.org/abs/2102.12702 .
+        When not using lazy attention, we can use residual attention (https://arxiv.org/abs/2012.11747) by giving this
+        module previous_attn_dots, which are the dots from the last attention layer.
+        """
+        super().__init__()
+
+        # Config
+        dim_head = int(attn_dim / num_heads)
+        assert attn_dim % num_heads == 0, "The attention dimension size (attn_dim) must divide evenly into num_heads"
+
+        self.scale = dim_head ** -0.5
+        self.num_heads = num_heads
+        self.previous_attention_bool = previous_attention_bool
+        self.pre_norm_bool = pre_norm_bool
+        self.post_norm_bool = post_norm_bool
+        self.causal_mask_bool = causal_mask_bool
+        self.attn_dim = attn_dim
+        self.rotate_qk_bool = rotate_qk_bool
+        self.rotate_v_bool = rotate_v_bool
+
+        # Functions
+        if self.previous_attention_bool:
+            # If we use the attention pattern from the last attention layer, we don't need queries and keys
+            self.to_v = nn.Linear(dim, attn_dim, bias=False)
+
+        else:
+            # Standard attention layer that will calculate the attention pattern from queries and keys
+            self.to_q = nn.Linear(dim, attn_dim, bias=False)
+            self.to_k = nn.Linear(dim, attn_dim, bias=False)
+            self.to_v = nn.Linear(dim, attn_dim, bias=False)
+
+        if self.pre_norm_bool:
+            self.pre_norm = nn.LayerNorm(dim)
+
+        if self.post_norm_bool:
+            self.post_norm = nn.LayerNorm(dim)
+
+        self.attn_fn = F.softmax
+        self.to_out = nn.Linear(attn_dim, dim)
+
+    @typechecked
+    def _calculate_attention_map(self,
+                                 _q: TensorType["batch", "num_heads", "length_queries", "head_dim"],
+                                 _k: TensorType["batch", "num_heads", "length_keys", "head_dim"],
+                                 _rope: Optional[TensorType[1, 1, "max_length", "rope_dim"]],
+                                 ) \
+            -> TensorType["batch", "num_heads", "length_queries", "length_keys"]:
+
+        _q = self.to_q(_q)  # create queries via a linear projection
+        _k = self.to_k(_k)  # create keys via a linear projection
+
+        # For q, k, and v, rearrange the features into heads (hence the name multi-headed attention)
+        _q, _k, = map(lambda t: rearrange(t, 'batch length (num_heads head_dim) -> batch num_heads length head_dim',
+                                             num_heads=self.num_heads), (_q, _k))
+
+        if self.rotate_qk_bool:
+            assert exists(_rope), "Layer must be given RoPE (rotary embeddings) if rotate_qk_bool is True"
+            # Apply a rotation to queries and keys which encodes positional information via their dot product
+            # Resulting dot products will be higher (more similar) the closer they are to each other in the sequence
+
+            _q = apply_rotary_pos_emb(x=_q, frequencies=_rope)
+            _k = apply_rotary_pos_emb(x=_k, frequencies=_rope)
+
+        # Perform a dot product between the queries and keys, along the feature dimension. The resultant tensor,
+        # dots, is a measure of similarity between the features, for each head
+        # TBD: Explain einsum notation, a bit
+        _dots = einsum('b h i d, b h j d -> b h i j', _q, _k) * self.scale
+
+        return _dots
+
+    @typechecked
+    def _weighted_sum(self,
+                      _v: TensorType["batch", "num_heads", "length_keys", "head_dim"],
+                      _attn_map: TensorType["batch", "num_heads", "length_queries", "length_keys"],
+                      _rope: Optional[TensorType[1, 1, "max_length", "rope_dim"]],
+                      ) \
+            -> TensorType["batch", "num_heads", "length_queries", "head_dim"]:
+
+        _v = self.to_v(_v)  # create values via a linear projection
+
+        # For q, k, and v, rearrange the features into heads (hence the name multi-headed attention)
+        _v = rearrange(_v, 'batch length (num_heads head_dim) -> batch num_heads length head_dim',
+                       num_heads=self.num_heads)
+
+        if self.rotate_v_bool:
+            assert exists(_rope), "Layer must be given RoPE (rotary embeddings) if rotate_v_bool is True"
+            _v = apply_rotary_pos_emb(x=_v, frequencies=_rope)
+
+        _v = einsum('b h i j, b h j d -> b h i d', _attn_map, _v)  # Weighted sum of value heads based on attn_map
+
+        return _v
+
+    @typechecked
+    def forward(self,
+                x: TensorType["batch", "length_queries", "dim"],  # Layer input
+                context: Optional[TensorType["batch", "length_keys", "dim"]] = None,  # Keys/values or memory
+                mask=None,
+                positional_bias_fn=None,  # Positional bias which is added to dots
+                previous_attn_map=None,  # The attention map (after softmax) from the last attention calculation
+                previous_attn_dots=None,  # The attention dots (before softmax) from the last attention calculation
+                rotary_pos_emb=None,  # RoPE embeddings
+                ):
+
+        residual = x  # Store input
+
+        if self.pre_norm_bool:
+            x = self.pre_norm(x)  # Normalize the representations before attention
+
+        if exists(previous_attn_map) and self.previous_attention_bool:  # Re-use the last attention map
+            if exists(context):
+                v_input = context  # Used for cross-attention
+            else:
+                v_input = x  # Used for self-attention
+
+            v = self.to_v(v_input)  # Create values via a linear projection
+            attn_map = previous_attn_map  # Set the current attention map to the last map (includes last mask)
+            dots = None  # We did not calculate any attention dots
+
+        elif not self.previous_attention_bool:  # Calculate attention map from queries and keys
+            if exists(context):
+                # For cross-attention, the layer's input are queries, and the keys/values come from the context
+                q_input = x
+                k_input = context
+                v_input = context
+            else:
+                # For self-attention, the qkv inputs all start from x (this layer's input)
+                q_input = x
+                k_input = x
+                v_input = x
+
+            dots = self._calculate_attention_map(_q=q_input, _k=k_input, _rope=rotary_pos_emb)
+
+            if exists(previous_attn_dots):
+                dots = dots + previous_attn_dots  # Add attention dots residual connection
+
+            if exists(positional_bias_fn):
+                dots = positional_bias_fn(dots)  # Apply a positional bias to the attention map
+
+            if exists(mask):
+                dots = dots + mask  # Add negative infinity to masked positions, and 0 elsewhere
+
+            if self.causal_mask_bool:
+                # This is Lucid's implementation
+                # Creating the mask at model initialization is faster (0.2 - 0.5% in my experiments) but I haven't
+                # found a way to keep the flexibility of this method, with respect to device and dtype
+
+                mask_value = max_neg_value(dots)  # the maximum negative number PyTorch allows, for this dtype
+                i, j = dots.shape[-2:]  # since the dots have shape (batch_size, num_heads, q_length, kv_length)
+                r = torch.arange(i, device=dots.device)  # count up to the number of queries
+                mask = rearrange(r, 'i -> () () i ()') < rearrange(r, 'j -> () () () j')  # creates a matrix that is
+                # true iff the row number is less than the column number (so it's true in the upper triangle)
+
+                if i != j:
+                    # This is only required when the query length is not equal to the key/value length, for example,
+                    # when we are generating using a key/value cache (and the query length is 1)
+                    mask = F.pad(mask, (j - i, 0), value=False)  # pad j - i columns to the left of the mask with false
+
+                dots.masked_fill_(mask, mask_value)  # replace the dots value with negative inf, where mask is true
+
+            attn_map = self.attn_fn(dots, dim=-1)  # Take the softmax over the length of the sequence (keys/values)
+
+        else:
+            raise "If self.previous_attention_bool is True, previous_attn_map needs to be given"
+
+        x = einsum('b h i j, b h j d -> b h i d', attn_map, v)  # Weighted sum of value heads based on attn_map
+        x = rearrange(x, 'b h n d -> b n (h d)')  # Merge the heads back together so we have the same number of
+        # features as our input
+
+        x = self.to_out(x)  # Send through a final linear projection
+        x = x + residual  # Add the layer's input to create a residual/skip connection
+
+        if self.post_norm_bool:
+            x = self.post_norm(x)  # Normalize the representations after attention
+
+        return x, attn_map, dots  # Return the output, attention map, and the dots (in case we need them later)
+
+
+class OldAttention(nn.Module):
     def __init__(self,
                  dim,  # Input and output dimension size (typically it is d_model)
                  attn_dim,  # Dimension size of attention (typically it is equal to dim)
--- positional_and_masking_utils.py
+++ positional_and_masking_utils.py
@@ -3,6 +3,11 @@ import torch
 from torch import nn, einsum
 from einops import rearrange, repeat, reduce
 import math
+from torchtyping import TensorType, patch_typeguard
+from typeguard import typechecked
+
+
+patch_typeguard()
 
 
 def exists(val):
@@ -87,36 +92,56 @@ class Alibi(nn.Module):
 
 class RotaryEmbedding(nn.Module):
     def __init__(self, dim):
-        super().__init__()
         """
         This rotary embedding (RoPE) implementation is taken 100% from lucidrains. Explanation forthcoming.
         :param dim: number of features to rotate
         """
+        super().__init__()
 
+        # Create a frequency for every second dimension, start with f = 1.0, divide by 10 to get the next, and repeat
+        # For example, if dim = 8, inv_freq = [1.0, 0.1, 0.01, 0.001]
         inv_freq = 1. / (10000 ** (torch.arange(0, dim, 2).float() / dim))
         self.register_buffer('inv_freq', inv_freq)
 
     def forward(self, max_seq_len):
-        t = torch.arange(max_seq_len).type_as(self.inv_freq).cuda()
-        freqs = torch.einsum('i , j -> i j', t, self.inv_freq)
-        emb = torch.cat((freqs, freqs), dim=-1)
+        t = torch.arange(max_seq_len).type_as(self.inv_freq).cuda()  # count up from 0 to (max_seq_len - 1)
+        freqs = torch.einsum('i , j -> i j', t, self.inv_freq)  # multiply t with inv_freq, shape (max_seq_len, dim/2)
+        emb = torch.cat((freqs, freqs), dim=-1)  # repeat freqs once, shape (max_seq_len, dim)
         return rearrange(emb, 'n d -> () () n d')
 
 
-def rotate_half(x):
-    # This implementation is taken 100% from lucidrains
-    # Explanation forthcoming
-    x = rearrange(x, '... (j d) -> ... j d', j=2)
-    x1, x2 = x.unbind(dim=-2)
-    return torch.cat((-x2, x1), dim=-1)
+@typechecked
+def rotate_half(x: TensorType["batch", "num_heads", "length", "dim"]) \
+        -> TensorType["batch", "num_heads", "length", "dim"]:
+
+    x = rearrange(x, '... (j d) -> ... j d', j=2)  # split the features into two
+    x1, x2 = x.unbind(dim=-2)  # separate them
+    return torch.cat((-x2, x1), dim=-1)  # rotate one of them (multiplying by negative 1), return the concatenation
 
 
-def apply_rotary_pos_emb(t, freqs):
-    # This implementation is taken 100% from lucidrains
-    # Explanation forthcoming
-    seq_len = t.shape[-2]
-    freqs = freqs[:, :, -seq_len:]
-    return (t * freqs.cos()) + (rotate_half(t) * freqs.sin())
+@typechecked
+def apply_rotary_pos_emb(x: TensorType["batch", "num_heads", "length", "dim"],
+                         frequencies: TensorType[1, 1, "max_length", "dim"]) \
+        -> TensorType["batch", "num_heads", "length", "dim"]:
+
+    num_features = frequencies.shape[-1]  # The number of features we wish to rotate
+    x_rotate = x[..., :num_features]  # Features to rotate
+    x_orig = x[..., num_features:]  # Features to keep, as is
+
+    seq_len = x_rotate.shape[-2]  # Length of the input
+    frequencies = frequencies[:, :, -seq_len:]  # Take the frequencies we need (just up to seq_len)
+    x_rotate = (x_rotate * frequencies.cos()) + (rotate_half(x_rotate) * frequencies.sin())  # Apply rotation
+
+    x = torch.cat([x_rotate, x_orig], dim=-1)  # Piece back together
+    return x
+
+
+# def old_apply_rotary_pos_emb(t, freqs):
+#     # This implementation is taken 100% from lucidrains
+#     # Explanation forthcoming
+#     seq_len = t.shape[-2]
+#     freqs = freqs[:, :, -seq_len:]
+#     return (t * freqs.cos()) + (rotate_half(t) * freqs.sin())
 
 
 """
