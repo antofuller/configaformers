1219eedaa83fcbec114c45fba04193d65b48aaac
muddyrains <afuller187187@gmail.com>
2021-09-14 23:13:29 -0400

    modeling config

--- modeling.py
+++ modeling.py
@@ -1,9 +1,16 @@
 import torch.nn as nn
-from building_blocks import FFN, NewAttention
+from building_blocks import FFN, Attention
 from positional_and_masking_utils import RotaryEmbedding
 import torch
-from einops import rearrange, repeat, reduce
+import inspect
 
+def get_default_args(func):
+    signature = inspect.signature(func)
+    return {
+        k: v.default
+        for k, v in signature.parameters.items()
+        if v.default is not inspect.Parameter.empty
+    }
 
 """
 *** Basic implementation to test building blocks ***
@@ -14,39 +21,60 @@ def max_neg_value(tensor):
     return -torch.finfo(tensor.dtype).max
 
 
-class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, vocab_sz):
-        super().__init__()
-        self.layers = nn.ModuleList([])
+def build_layer(layer_config, _dim_model):
+    if layer_config["type"] == "Attention":
+        default_args = get_default_args(Attention)
 
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                NewAttention(dim=dim,
-                             attn_dim=dim,
-                             num_heads=heads,
-                             ),
-                FFN(dim,
-                    ff_mult=4),
-            ]))
+        input_dict = {}
+        for key in default_args:
+            if key == "type":
+                continue
+            if key not in layer_config.keys():
+                input_dict[key] = default_args[key]
+            else:
+                input_dict[key] = layer_config[key]
+
+        return Attention(dim=_dim_model,
+                         **input_dict,
+                         )
+
+
+class Transformer(nn.Module):
+    def __init__(self, config):
+        super().__init__()
 
         # Config
-        self.dim = dim
-        self.vocab_size = vocab_sz
-        self.num_layers = depth
-        self.heads = heads
-        dim_head = dim // heads
+        self.dim_model = config['dim_model']
+        self.vocab_size = config['vocab_size']
+        self.num_layers = len(config['layers'])
+        self.dim_rope = config['dim_rope']
+
+        self.layers = nn.ModuleList([])
+        for layer_id in range(self.num_layers):
+            self.layers.append(nn.ModuleList([
+                build_layer(layer_config=config['layers'][layer_id], _dim_model=self.dim_model),
+            ]))
+        # for layer_id in range(self.num_layers):
+        #     self.layers.append(nn.ModuleList([
+        #         Attention(dim=dim,
+        #                   attn_dim=dim,
+        #                   num_heads=heads,
+        #                   ),
+        #         FFN(dim,
+        #             ff_mult=4),
+        #     ]))
 
         # Input utils
-        self.token_emb = nn.Embedding(self.vocab_size, self.dim)
-        self.rotary_pos_emb = RotaryEmbedding(int(dim_head/4))
+        self.token_emb = nn.Embedding(self.vocab_size, self.dim_model)
+        self.rotary_pos_emb = RotaryEmbedding(self.dim_rope)
 
-        self.logits_input_norm = nn.LayerNorm(dim)
-        self.to_logits = nn.Linear(self.dim, self.vocab_size)
+        self.logits_input_norm = nn.LayerNorm(self.dim_model)
+        self.to_logits = nn.Linear(self.dim_model, self.vocab_size)
 
     def forward(self, seq_ids):
         bsz = seq_ids.shape[0]
         seq_len = seq_ids.shape[1]
-        x = self.token_emb(seq_ids).view(bsz, seq_len, self.dim)
+        x = self.token_emb(seq_ids).view(bsz, seq_len, self.dim_model)
         rotary_pos_emb = self.rotary_pos_emb(seq_len)
 
         for self_attn, self_ff in self.layers:
