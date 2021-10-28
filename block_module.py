8ffa2ded55f8dabb06191e66b672c89e17dddaed
muddyrains <afuller187187@gmail.com>
2021-10-28 17:26:08 -0400

    block module

---
+++ block_module.py
@@ -0,0 +1,54 @@
+import torch.nn.functional as F
+import torch
+from torch import nn, einsum
+from einops import rearrange, repeat, reduce
+import math
+
+from typing import Optional, Tuple, Union, List, Dict
+from linear_module import LinearProj
+from activation_module import Activation
+
+
+def get_block(_type):
+    _type = _type.lower()  # Make lowercase
+    if _type == "activation":
+        return Activation
+
+    elif _type == "linear":
+        return LinearProj
+
+    else:
+        raise "Layer type does not match any available types."
+
+
+class Block(nn.Module):
+    def __init__(self,
+                 block_config,
+                 ):
+        super().__init__()
+        # Type checking
+        assert type(block_config) == list, f"Block's config should be a list, it was given a {type(block_config)}"
+        for module_config in block_config:
+            assert type(module_config) == dict, f"Block's config should be a list of dicts, it was given a" \
+                                                f" {type(module_config)}, inside the list."
+
+        # Build block by iterating over modules
+        self.modules = nn.ModuleList([])
+        for module_config in block_config:
+            assert 'type' in module_config.keys(), f'Module not given a type'
+            assert type('type') == str,  f"Module's type needs to be a string."
+
+            block = get_block(_type=module_config['type'])
+
+            self.modules.append(block(module_config))
+
+    def forward(self, x):
+
+        for module in enumerate(self.modules):
+            x = module(x)
+
+        return x
+
+
+
+
